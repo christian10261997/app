bff4bcfc948d20ba1c60e1b997a52dbceb09ebf1
christian10261997 <chanbringas577@gmail.com>
2025-08-24 15:53:02 +0800

    update firebase

--- sampleapp/config/firebase.ts
+++ sampleapp/config/firebase.ts
@@ -1,18 +1,18 @@
 import { default as ReactNativeAsyncStorage } from "@react-native-async-storage/async-storage";
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAuth, getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { Auth, getAuth, getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // Your Firebase configuration
 // Replace these values with your actual Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyByQtwAn_4jYedE6ZQNYGMUs9uiJV58Z-8",
-  authDomain: "kitchenpal-testing.firebaseapp.com",
-  projectId: "kitchenpal-testing",
-  storageBucket: "kitchenpal-testing.firebasestorage.app",
-  messagingSenderId: "412336730027",
-  appId: "1:412336730027:web:4d8d74268030a97529ecee",
-  measurementId: "G-PPJ3QHTT5K",
+  apiKey: "AIzaSyBUtc1mqoZAO-Wyokq60AwB2XZ9F7OH6Eo",
+  authDomain: "kitchenpal-49502.firebaseapp.com",
+  projectId: "kitchenpal-49502",
+  storageBucket: "kitchenpal-49502.firebasestorage.app",
+  messagingSenderId: "543039232336",
+  appId: "1:543039232336:web:8045653afb4ddb403c3455",
+  measurementId: "G-GFWRE5KZ5R"
 };
 
 // Initialize Firebase App
@@ -21,7 +21,7 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 // Create a custom persistence object that mimics what Firebase expects
 
 // Initialize Firebase Auth with custom persistence
-let auth;
+let auth: Auth;
 try {
   auth = initializeAuth(app, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage),
